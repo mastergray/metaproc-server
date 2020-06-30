3779c99b01b9d692d0bb9529a32d1eb584da3b0d
mastergray <mastergray@users.noreply.github.com>
2020-06-29 23:34:19 -0500

    Refactored everything to work with newer version of METAPROC.

--- index.js
+++ index.js
@@ -4,183 +4,155 @@ const myIP = require('my-ip');              // Shows IP of where server is runni
 const express = require('express');         // Handles HTTP routes, requests, and responses
 const helmet = require('helmet');           // Basic server security
 const EventEmitter = require('events');     // To help process requests without blocking the event loop
-const METAPROC = require("metaproc");       // How this all gets structured
+const METAPROC = require("/home/mastergray/dev/metaproc-js/index.js");       // How this all gets structured
 
-// MAIN PROCESS :: () -> SERVER
-// Creates METAPROC interface for implementing an HTTP server leveraging Express.j
-module.exports = SERVER = (fns) => METAPROC.init([
-  METAPROC.ops,
-  SERVER.ops,
-  SERVER.methods
-])
+// SERVER :: (STATE, OPS) -> SERVER
+// HTTP Server implmentation using METAPROC and EXPRESS
+module.exports = SERVER = (FNS, OPS) => METAPROC.Standard(FNS, OPS)
 
-/**
- *
- *  "Static" Methods
- *
- */
+  /**
+   *
+   *  Route Operations
+   *
+   */
 
-// OBJECT -> OBJECT
-// Consoles out address of server with port number:
-// NOTE: This can only be applied after "start" has been called:
-SERVER.showAddress = (STATE) => {
-  console.log(`http://${STATE.address}:${STATE.port}`);
-  return STATE;
-}
-
-
-/**
- *
- *  Operations
- *
- */
-
-SERVER.ops = [
-
-  // GET :: (STRING, (STATE, REQUEST, RESPONSE, NEXT)) -> PROMISE(STATE)
-  // Binds "GET" route to EXPRESS instance stored in STATE:
-  METAPROC.OP("GET",(route, fn) => addRoute("get", route, fn)),
-
-  // POST :: (STRING, (STATE, REQUEST, RESPONSE, NEXT)) -> PROMISE(STATE)
-  // Binds "POST" route to EXPRESS instance stored in STATE:
-  METAPROC.OP("POST", (route, fn) => addRoute("post", route, fn)),
-
-  // DELETE :: (STRING, (STATE, REQUEST, RESPONSE, NEXT)) -> PROMISE(STATE)
-  // Binds "DELETE" route to EXPRESS instance stored in STATE:
-  METAPROC.OP("DELETE", (route, fn) => addRoute("delete", route, fn)),
-
-  // OPTIONS :: (STRING, (STATE, REQUEST, RESPONSE, NEXT)) -> PROMISE(STATE)
-  // Binds "OPTIONS" route to EXPRESS instance stored in STATE:
-  METAPROC.OP("OPTIONS", (route, fn) => addRoute("options", route, fn)),
-
-  // PUT :: (STRING, (STATE, REQUEST, RESPONSE, NEXT)) -> PROMISE(STATE)
-  // Binds "PUT" route to EXPRESS instance stored in STATE:
-  METAPROC.OP("PUT", (route, fn) => addRoute("put", route, fn)),
-
-  // when :: (STRING, (EVENT) -> VOID) -> PROMISE(STATE)
-  // Create event listener using instance of EVENTEMITTER  stored in STATE:
-  METAPROC.OP("when", (evt, fn) => addEventHandler(evt, fn)),
-]
-
-/**
- *
- *  Methods
- *
- */
-
- SERVER.methods = [
-
-   // Takes functions created from SERVER operations and chains them to a "standard" instance of metaproc,
-   // this is so the order of functions applied to STATE include running server setup and start at the right time:
-   // NOTE: Config passed to "start" method WILL be mutated by this new instance:
-   METAPROC.METHOD("start", (config) => (metaproc) => {
-     return METAPROC.Standard()
-       .aptoifnot("postLimit", (STATE) => "10mb")      // Default postLimit of "10mb",
-       .aptoifnot("port", (STATE) => 3000)             // Default port number of 3000
-       .aptoifnot("CORS", STATE => [])                 // Default CORS
-       .aptoifnot("address", (STATE) => undefined)     // Default address is undefined
-       .apto("events", (STATE) => new EventEmitter())  // Handles events between requests)
-       .apto("app", (STATE) => express())              // Initalize instance of express to operate on
-       .apto("app", setupServer)                       // Setup server using config
-       .chain(metaproc)                                // Chain functions from instance
-       .apto("app", startServer)                       // Start server using config
-       .run(config)
-     .then((STATE) => ({                               // Returns intialized config
-       "postLimit":STATE.postLimit,
-       "port":STATE.port,
-       "CORS":STATE.CORS,
-       "address":STATE.address||myIP()
-     }))
-   })
-
- ]
-
-/**
- *
- *  Subprocesses
- *
- */
-
-// :: (express, STATE) -> express
-// Setup express server stored in STATE:
-function setupServer(app, STATE) {
-
-  // Initialize "helmet" for standard security:
-  // https://expressjs.com/en/advanced/best-practice-security.html
-  app.use(helmet());
-
-  // Here we are configuring express to use body-parser as middle-ware:
-  app.use(bodyParser.urlencoded({limit: STATE.postLimit, extended: true}));
-  app.use(bodyParser.json({limit: STATE.postLimit, extended: true}));
-
-  // Here we are configuring CORS:
-  app.use((req, res, next) => {
-    if (STATE.CORS.length > 0) {
-      if (STATE.CORS.includes(req.headers.origin)) {
-        res.header("Access-Control-Allow-Origin", req.headers.origin)
-        res.header("Access-Control-Allow-Headers", "*");
-        res.header("Access-Control-Allow-Methods", "*");
-      } else {
-        res.status(401)
-      }
-    }
-    next();
-  });
-
-  return app;
-
-}
-
-// :: (express, STATE) -> express
-// Start Express server:
-function startServer(app, STATE) {
-
-  // File not found route:
-  app.use("*", (req, res, next) => {
-    res.status(404).send("Route Does Not Exist")
-  })
-
-  // Catch all error handler:
-  // NOTE: This is triggered by next(err):
-  app.use((err, req, res, next) => {
-    console.error(err);
-    console.error(`UNCAUGHT ERROR: ${err}`);
-    res.status(500).send("Internal Server Error");
-  });
-
-  // Start listening....
-  http.Server(app).listen(STATE.port, STATE.address);
-
-  // Returns app to be bound to STATE:
-  return app;
-
-}
-
-/**
- *
- *  Support Functions
- *
- */
-
-// :: (STRING, STRING, (STATE, REQUEST, RESPONSE, NEXT) -> VOID) -> STATE -> VOID
-// Binds function to route of express instance:
-function addRoute(method, route, fn) {
-  return (STATE) => {
-    STATE.app[method](route, (req, res, next) =>{
-      fn(STATE, req, res, next);
-    })
-    return STATE;
-  }
-}
-
-// :: (STRING, (evt, *) -> VOID) -> VOID
-// Binds event handlers to event emitter stored in STATE.evente:
-// NOTE:  First argument of any handler is always STATE:
-function addEventHandler(evt, fn) {
-  return (STATE) => {
-    STATE.events.on(evt, (...vals) => {
-      fn.apply(null, [STATE].concat(vals));
+  // addRoute :: (STRING, STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRSS.NEXT) -> VOID) -> METAPROC
+  // Binds route to EXPRESS instance stored in STATE:
+  .augment("addRoute", (method, route, fn) => (metaproc) => metaproc.apto("app", (app, state) => {
+    app[method](route, (req, res, next) => {
+      fn(state, req, res, next)
     });
-    return STATE;
-  }
-}
+    return app;
+  }))
+
+  // GET :: ((STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "GET" route to EXPRESS instance stored in STATE:
+  .augment("GET", (route, fn) => (metaproc) => metaproc.addRoute("get", route, fn))
+
+  // POST :: ((STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "POST" route to EXPRESS instance stored in STATE:
+  .augment("POST", (route, fn) => (metaproc) => metaproc.addRoute("post", route, fn))
+
+  // PUT :: ((STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "PUT" route to EXPRESS instance stored in STATE:
+  .augment("PUT", (route, fn) => (metaproc) => metaproc.addRoute("put", route, fn))
+
+  // DELETE :: ((STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "DELETE" route to EXPRESS instance stored in STATE:
+  .augment("DELETE", (route, fn) => (metaproc) => metaproc.addRoute("delete", route, fn))
+
+  // OPTIONS :: ((STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "OPTIONS" route to EXPRESS instance stored in STATE:
+  .augment("OPTIONS", (route, fn) => (metaproc) => metaproc.addRoute("options", route, fn))
+
+  // use :: (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRESS.NEXT) -> VOID) -> METAPROC
+  // Binds "middleware" to EXPRSS instance stored in STATE:
+  // addRoute :: (STRING, STRING, (STATE, EXPRESS.REQ, EXPRESS.RES, EXPRSS.NEXT) -> VOID) -> METAPROC
+  // Binds route to EXPRESS instance stored in STATE:
+  .augment("use", (method, route, fn) => (metaproc) => metaproc.apto("app", (app, state) => {
+    app.use((req, res, next) => {
+      fn(state, req, res, next)
+    });
+    return app;
+  }))
+
+  /**
+   *
+   *  Event Operations
+   *
+   */
+
+  // when :: (STRING, (EVENT) -> VOID) -> METAPROC
+  // Creates an event listener for instance of EVENTEMITTER stored in STATE:
+  .augment("when", (evt, fn) => (metaproc) =>  metaproc.apto("events", (events, state) => {
+    events.on(evt, (...vals) => {
+      fn.apply(null, [state].concat(vals));
+    })
+    return events;
+  }))
+
+  /**
+   *
+   *  Server Initialization Operations
+   *
+   */
+
+  // :: (VOID) -> (METAPROC) -> METAPROC
+  // Setup express server stored in STATE:
+  // NOTE: If this needs to be modified, it can be overwrriten before applied by "init" OP:
+  .augment("setup", () => (metaproc) => metaproc.apto("app", (app, state) => {
+
+    // Initialize "helmet" for standard security:
+    // https://expressjs.com/en/advanced/best-practice-security.html
+    app.use(helmet());
+
+    // Here we are configuring express to use body-parser as middle-ware:
+    app.use(bodyParser.urlencoded({limit: state.postLimit, extended: true}));
+    app.use(bodyParser.json({limit: state.postLimit, extended: true}));
+
+    // Here we are configuring CORS:
+    app.use((req, res, next) => {
+      if (state.CORS.length > 0) {
+        if (state.CORS.includes(req.headers.origin)) {
+          res.header("Access-Control-Allow-Origin", req.headers.origin)
+          res.header("Access-Control-Allow-Headers", "*");
+          res.header("Access-Control-Allow-Methods", "*");
+        } else {
+          res.status(401)
+        }
+      }
+      next();
+    });
+
+    // Return app
+    return app;
+
+  }))
+
+  // :: (VOID) -> (METAPROC) -> METAPROC
+  // Start Express server:
+  // NOTE: If this needs to be modified, it can be overwrriten before applied by "create" OP:
+  .augment("start", (banner) => (metaproc) => metaproc.apto("app", (app, state) => {
+
+    // File not found route:
+    app.use("*", (req, res, next) => {
+      res.status(404).send("Route Does Not Exist")
+    })
+
+    // Catch all error handler:
+    // NOTE: This is triggered by next(err):
+    app.use((err, req, res, next) => {
+      console.error(err);
+      console.error(`UNCAUGHT ERROR: ${err}`);
+      res.status(500).send("Internal Server Error");
+    });
+
+    // Start listening....
+    http.Server(app).listen(state.port, state.address);
+
+    // Returns app to be bound to STATE:
+    return app;
+
+  }))
+
+  // create :: (OBJECT, (STATE) -> VOID) -> METAPROC
+  // Initializes EXPRESS server with given CONFIG object ,
+  // supports optional banner METHOD for printing message to console  once server is running
+  .augment("create", (config, banner) => (metaproc) => {
+    return SERVER()
+      .asifnot("postLimit", "10mb")           // Default postLimit of "10mb",
+      .asifnot("port", 3000)                  // Default port of "3000"
+      .asifnot("CORS", [])                    // Default CORS
+      .asifnot("address", undefined)          // Default address
+      .asifnot("app", express())              // Initalize instance of express to operate on
+      .asifnot("events", new EventEmitter())  // Handles events between requests)
+      .setup()                                // Initialze server
+      .chain(() => metaproc)                  // Initialize routes
+      .start()                                // Start server
+      .run(config)
+    .then((state) => {
+      state.address = state.address || myIP(); // So IP can be referenced from routes
+      console.log(banner !== undefined ? banner(state) : `Running @ http://${state.address}:${state.port}`)
+      return state;
+    })
+  })
--- test/index.js
+++ test/index.js
@@ -1,12 +1,49 @@
-const SERVER = require("../index.js")
+const SERVER = require("../index2.js")
 
-SERVER_A = SERVER().GET("/", (STATE, req, res, next) => {
+SERVER_A =  (STATE) => SERVER(STATE).GET("/", (STATE, req, res, next) => {
+  let html = Object.keys(STATE.visits).reduce((html, link) => {
+    html += `<li><a href="${link}">${link}</a></li>`;
+    return html;
+  }, "<h1>WELCOME!!!!111</h1><ul>") + "</ul>";
+  res.send(html);
+});
+
+
+SERVER_B =  (STATE) => SERVER(STATE).GET("/test", (STATE, req, res, next) => {
+  res.send("test!");
+});
+
+SERVER_C =  (STATE) => SERVER(STATE).GET("/hello", (STATE, req, res, next) => {
   res.send("hello world!");
 });
 
-SERVER_B =  SERVER().GET("/test", (STATE, req, res, next) => {
-  res.send("test!");
-});
+ECHO_SERVER = (STATE) => SERVER(STATE)
+  .augment("echo", (route) => (metaproc) =>  {
+    return metaproc.GET(route, async (STATE, req, res, next) => {
+      res.send(`http://${STATE.address}:${STATE.port}`)
+    })
+  })
+  .echo("/echo")
+
+WATCHER = (STATE) => SERVER(STATE)
+.asifnot("visits", {"/":0, "/test":0, "/echo":0, "/visits":0})
+.GET("/visits", (STATE, req, res, next) => {
+  let html = Object.keys(STATE.visits).reduce((html, whereFrom) => {
+    html += `<tr><td align="center">${whereFrom}</td><td align="center">${STATE.visits[whereFrom]}</td></tr>`;
+    return html;
+  }, `<table width="100%" border="1"><tr><th>Page Name</th><th>Number of Visits</th></tr>`) + "</table>";
+  res.send(html);
+})
+.when("visit", (STATE, whereFrom) => {
+  if (STATE.visits[whereFrom] !== undefined) {
+    STATE.visits[whereFrom] += 1;
+  } else {
+    console.log(`Visit not counted for ${whereFrom}`)
+  }
+
+})
+
+/*
 
 SERVER()
   .GET("/*", (STATE, req, res, next) => {
@@ -18,3 +55,21 @@ SERVER()
   .start({"port":3005})
 .then(SERVER.showAddress)
 .catch((err) => console.log(err))
+
+*/
+
+SERVER()
+.GET("/*", (STATE, req, res, next) => {
+  console.log(req.originalUrl, req.connection.remoteAddress)
+  STATE.events.emit("visit", req.originalUrl);
+  next();
+})
+.chain(SERVER_A)
+.chain(SERVER_B)
+.chain(SERVER_C)
+.chain(ECHO_SERVER)
+.chain(WATCHER)
+.create({"port":3005})
+.catch((err) => {
+  console.log(err.msg)
+})
